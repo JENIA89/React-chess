6f384745a41c69da68ef2c1e0cb3df5df015c484
Eugene Popolamov <eugene.popolamov@effective-soft.com>
2022-05-22 20:12:06 +0300

    feat: create board

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import BoardComponent from './components/BoardComponent';
+import { Board } from './models/Board';
 
 function App() {
+  const [board, setBoard] = useState(new Board())
+
+  useEffect(() => {
+    restart()
+  }, [])
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells()
+    setBoard(newBoard)
+  }
+
   return (
-    <div className="App">
-      CHESS
+    <div className="app">
+      <BoardComponent board={board} setBoard={setBoard}/>
     </div>
   );
 }
